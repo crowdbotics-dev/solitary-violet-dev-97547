ac0923041f8c8487de8d38e34aa3579cffbe4aab
Crowdbotics <engineering@crowdbotics.com>
2023-07-05 05:52:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solitary-violet-dev-97547",
+  url: "https://solitary-violet-dev-97547.botics.co",
+  api: "https://solitary-violet-dev-97547.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
